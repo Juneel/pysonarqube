6b475595ff5ba6ebf1de5c536ba1f4262d4a80f4
lijun01 <lijun01@qiyi.com>
2019-09-30 14:03:35 +0800

    类装饰器，打印日志

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ user: 用户的类,封装了用户的属性和方法
 
 measure: 工程的扫描结果的类，封装了获取结果的方法
 
+log：打印日志的装饰器
+
 MRO：Measure>Component>User>Sonarqube>object
--- log.py
+++ log.py
@@ -8,10 +8,13 @@ import logging
 logger = logging.getLogger(name="sonar")
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler = logging.FileHandler("pysonarqube.log", mode='a')
-file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+file_handler_info = logging.FileHandler("pysonarqube.info", mode='a')
+file_handler_info.setLevel(logging.INFO)
+file_handler_info.setFormatter(formatter)
+logger.addHandler(file_handler_info)
+file_handler_error = logging.FileHandler("pysonarqube.error", mode='a')
+file_handler_error.setLevel(logging.ERROR)
+file_handler_error.setFormatter(formatter)
 
 
 def cls_log_handler(cls):
